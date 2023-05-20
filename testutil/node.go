63812d3618b9c279501b3673bd3fbbf67515147c
Andrii Tarykin <ksbomj@gmail.com>
2023-05-21 00:53:53 +0300

    Update node.go

--- testutil/node.go
+++ testutil/node.go
@@ -27,6 +27,16 @@ func StartBlockchain(tag string) (*Container, error) {
 	return c, nil
 }
 
+// AddBalance adds balance to a OBADA node account.
+func AddBalance(t *testing.T, c *Container, addr string, amount string) error {
+	bytes, err := ExecInContainer(c.ID, "fullcored", "tx", "bank", "send", "testnode", addr, amount, "--chain-id=obada-testnet", "--keyring-dir=.fullcore/keys", "--keyring-backend=test", "--fees=200000rohi", "--yes")
+	if err != nil {
+		return fmt.Errorf("cannot add balance to address: %w: %s", err, string(bytes))
+	}
+
+	return nil
+}
+
 // StopBlockchain stops a running OBADA node instance.
 func StopBlockchain(t *testing.T, c *Container) {
 	if err := StopContainer(c.ID); err != nil {
